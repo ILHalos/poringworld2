b202d05e6d9adff93c1818092692687fb325dbe4
ILHalos <65180510+ILHalos@users.noreply.github.com>
2020-05-11 12:46:27 -0400

    Add files via upload

---
+++ Poring.World.Crawler-master/Poring.World.Crawler/Poring.World.Crawler/Utilities/Helper.Conf/Caminhos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Utilities
+{
+    public static class Caminhos
+    {
+        public static string CaminhoLog = @"Logs.txt";
+        public static string CaminhoItens = @"Itens.txt";
+        public static string URL_Poring_Crawler = "https://www.poring.world/";
+    }
+}
---
+++ Poring.World.Crawler-master/Poring.World.Crawler/Poring.World.Crawler/Utilities/Util/ManipuladorArquivoTexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Utilities.Files
+{
+    public static class ManipuladorArquivoTexto
+    {
+        public static void EscreverArquivo(string texto, string path)
+        {
+            using (TextWriter tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(texto);
+            }
+        }
+    }
+}
